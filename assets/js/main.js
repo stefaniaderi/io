d1f275f5aed2f943c3fb9d6341d0e80ea32e6726
stefaniaderi <deristefania@hotmail.it>
2023-05-11 15:14:56 +0200

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,48 @@ for (var i = 0; i < document.links.length; i++) {
       document.links[i].className = 'active';
     }
 }
+
+//SCROLLING HIGLIGHT TABLE OF CONTENTS
+function isElementInViewport (el) {
+    
+    //special bonus for those using jQuery
+    if (typeof $ === "function" && el instanceof $) {
+        el = el[0];
+    }
+    
+    var rect     = el.getBoundingClientRect(),
+        vWidth   = window.innerWidth || doc.documentElement.clientWidth,
+        vHeight  = window.innerHeight || doc.documentElement.clientHeight,
+        efp      = function (x, y) { return document.elementFromPoint(x, y) };     
+
+    // Return false if it's not in the viewport
+    if (rect.right < 0 || rect.bottom < 0 
+            || rect.left > vWidth || rect.top > vHeight)
+        return false;
+
+    // Return true if any of its four corners are visible
+    return (
+          el.contains(efp(rect.left,  rect.top))
+      ||  el.contains(efp(rect.right, rect.top))
+      ||  el.contains(efp(rect.right, rect.bottom))
+      ||  el.contains(efp(rect.left,  rect.bottom))
+    );
+}
+
+
+var handler = function(){
+  var visible = 0;
+  [1,2,3,4].forEach(function(id){
+    if(isElementInViewport($('#'+id))) visible = id;
+  })
+  
+  if(visible){
+    $('#index div').removeClass('active');
+    $('#index-'+visible).addClass('active');
+  }
+};
+
+
+
+//jQuery
+$(window).on('DOMContentLoaded load resize scroll', handler);
